b1c01c535341de5958447dc682bc6e04434968cb
pqzhen01 <50341620+pqzhen01@users.noreply.github.com>
2023-08-16 12:13:04 -0400

    disable nat_gateway in vpc

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "vpc" {
   #private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  #enable_nat_gateway = true
   #enable_vpn_gateway = true
 
   tags = {
@@ -59,7 +59,7 @@ resource "aws_security_group" "blog" {
   tags = {
     Terraform = "true"
   }
-  
+
   #vpc_id = data.aws_vpc.default.id
   vpc_id              = module.vpc.public_subnets[0]
 }
